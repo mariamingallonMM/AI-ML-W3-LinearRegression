3b1fbf8da2613ba32d531105597137fd1be3e6c8
mariamingallonMM <maria.mingallon@mottmac.com>
2021-01-28 23:02:10 +1300

    added plotting function start of ridge regression function refer to todo for missing functionality

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# AI-ML-W3-LinearRegression
+﻿# AI-ML-W3-LinearRegression
 ColumbiaX CSMM.102x Machine Learning Course. Week 3 Assignment on Linear Regression.
+
+
+## Instructions
+This assignment has two parts. The aim is to write a function that implements both.
+
+**PART 1:** In this part we will implement the ℓ2 -regularized least squares linear regression algorithm (aka ridge regression) which takes the form:
+
+wRR=argminw∥y−Xw∥^2+λ∥w∥2. 
+
+Write the code that takes in data 'y'  and 'X' and outputs 'wRR' for an arbitrary value of  'λ'.
+
+**PART 2:** In the same code, we will also implement the ***'active learning procedure'***. The assignment provides an arbitrary setting of  'λ'  and  'σ2'  and asks that you provide the first 10 locations you would measure from a set D={x}  given a set of measured pairs (y,X). Please look over the slides carefully to remind yourself about the sequential evolution of the sets D and (y,X).
+
+More details about the inputs we provide and the expected outputs are given below.
+
+## What you need to submit
+
+## Execute the program
+The following command shall execute your program
+$ python hw1_regression.py lambda sigma2 X_train.csv y_train.csv X_test.csv
+
+Note the following:
+- The name of the train and testing X and y datasets. 
+- The main .py file shall be named 'hw1_regression.py'.
+- The value of 'lambda' ("λ") above will be a non-negative integer (any non-negative integer that is chosen by the reviewers when running the code).
+- The value of sigma2 ("σ2") above will be an arbitrary positive number.
+
+The values of lambda and sigma2 will be input as strings. They are converted to numbers in the code for it to work. Note that all numbers should be double-precision floating-point format.
+
+The csv files that we will input into your code are formatted as follows:
+
+- X_train.csv: A comma separated file containing the covariates. Each row corresponds to a single vector  xi . The last dimension has already been set equal to 1 for all data.
+- y_train.csv: A file containing the outputs. Each row has a single number and the i-th row of this file combined with the i-th row of "X_train.csv" constitutes the training pair  (yi,xi) .
+- X_test.csv: This file follows exactly the same format as "X_train.csv". No response file is given for the testing data.
+
+## Expected Outputs from the program
+When executed, the code writes the output for both PART 1 & 2 to the files listed below. Note the formatting instructions given below. For an arbitrary chosen value of "λ" and "σ2", you will create the following two files containing:
+
+- wRR_[lambda].csv: A file where the value in each dimension of the vector wRR is contained on a new line. This file corresponds to your output for PART 1 above.
+- active_[lambda]_[sigma2].csv: A comma separated file containing the row index of the first 10 vectors you would select from X_test.csv starting with the measured values in X_train.csv and y_train.csv. Please make sure your indexing starts at 1 and not at 0 (i.e., the first row is index 1). This file should contain one line with a "," separating each index value. This file corresponds to your output for PART 2 above.
+
+For example, if  λ=2  and  σ2=3 , then the output files the code will create will be named: 
+"wRR_2.csv" and "active_2_3.csv"
+
+If your code then learns that w = [3.2; -3.6; 1.4; -0.7], then wRR_2.csv should look like:
+
+3.2
+-3.6
+1.4
+-0.7
+
+If  the first 10 index values you would choose to measure are 724, 12, 109, 42, 23, 96, 342, 594, 123, 414, then active_2_3.csv should look like:
+
+724,12,109,42,23,96,342,594,123,414
+
+## Note on Correctness
+
+Please note that for both of these problems, there is one and only one correct solution. Therefore, we will grade your output based on how close your results are to the correct answer. We strongly suggest that you test out your code on your own computer before submitting. The UCI Machine Learning Repository (http://archive.ics.uci.edu/ml/) has a good selection of datasets for regression. While you still may not have the ground truth, you can build confidence that the outputs of your code are reasonable. For example, you can verify that your vector  wRR  makes reasonable predictions and that your 10 selected measurement indexes are all unique.
+
+## Notes on data repositories
+The following datasets have been selected from the UCI Machine Learning Repository for use and testing of the code written for this assignment:
+
+- [Forest Fires Data Set](http://archive.ics.uci.edu/ml/datasets/Forest+Fires). This is a difficult regression task, where the aim is to predict the burned area of forest fires, in the northeast region of Portugal, by using meteorological and other data (see details [here](http://www.dsi.uminho.pt/~pcortez/forestfires)).
+- [Wine Quality Data Set](http://archive.ics.uci.edu/ml/datasets/Wine+Quality). Two datasets are included, related to red and white vinho verde wine samples, from the north of Portugal. The goal is to model wine quality based on physicochemical tests (see [Cortez et al., 2009](http://www3.dsi.uminho.pt/pcortez/wine/)).
+
+
+## Citations & References
+
+- [Forest Fires Data Set](http://archive.ics.uci.edu/ml/datasets/Forest+Fires) by P. Cortez and A. Morais. A Data Mining Approach to Predict Forest Fires using Meteorological Data. In J. Neves, M. F. Santos and J. Machado Eds., New Trends in Artificial Intelligence, Proceedings of the 13th EPIA 2007 - Portuguese Conference on Artificial Intelligence, December, Guimaraes, Portugal, pp. 512-523, 2007. APPIA, ISBN-13 978-989-95618-0-9.
+- [Wine Quality Data Set](http://archive.ics.uci.edu/ml/datasets/Wine+Quality) by P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis. Modeling wine preferences by data mining from physicochemical properties. In Decision Support Systems, Elsevier, 47(4):547-553, 2009.
+
+
+
---
+++ datasets/in/hw1_regression.py
@@ -0,0 +1,195 @@
+﻿"""
+This code implements a linear regression per week 3 assignment of the machine learning module part of Columbia University Micromaster programme in AI. 
+Written using Python 3.7
+"""
+
+# builtin modules
+import os
+#import psutil
+import requests
+import sys
+import math
+
+
+# 3rd party modules
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+
+
+def vis_data(data_x, data_y):
+
+    fig = go.Figure()
+
+    for col in data_x.columns:
+        fig.add_trace(go.Scatter(x=data_x[col], y=data_y,
+                                 mode='markers',
+                                 name="{}".format(col)))
+        
+    return fig.show()
+
+
+def vis_regression_(X, y):
+
+    #TODO: fix dimensionality of dataframe plotted per column as above for actual curve
+
+    fig = go.Figure()
+
+    for col in data_x.columns:
+        fig.add_trace(go.Scatter(x=data_x[col], y=data_y,
+                                 mode='markers',
+                                 name="{}".format(col)))
+
+
+
+    return fig.show()
+
+
+
+def get_data(source_file):
+
+# Define input and output filepaths
+    input_path = os.path.join(os.getcwd(),'datasets','in', source_file)
+
+    # Read input data
+    df = pd.read_csv(input_path)
+       
+    return df
+
+
+def split_data(df, ratio:float = 0.7):
+
+    """
+    Splits the data set into the training and testing datasets from the following inputs:
+
+    df: dataframe of the dataset to split
+    ratio : percentage by which to split the dataset; ratio = training data / all data;
+    e.g. a ratio of 0.70 is equivalent to 70% of the dataset being dedicated to the training data and the remainder (30%) to testing.
+
+    Outputs: X_train, y_train, X_test, y_test
+   
+    X_train: Each row corresponds to a single vector  xi . The last dimension has already been set equal to 1 for all data.
+    y_train: Each row has a single number and the i-th row of this file combined with the i-th row of "X_train" constitutes the training pair (yi,xi).
+    X_test: The remainder of the dataset not included already in the X_train dataframe. Same format as "X_train".
+    y_test: The remainder of the dataset not included already in teh y_train dataframe. Same format as "y_train".
+
+    """
+
+    rows = df.shape[0]
+    cols = df.shape[1]
+    rows_split = int(ratio * rows)
+
+    # split the dataset into train and test sets
+    # drop last column of X which will become 'y' vector
+    # TODO: add a column of '1' to the X matrix
+    df_X_train = df[df.columns[:-1]].loc[0 : rows_split] 
+    df_X_test = df[df.columns[:-1]].loc[rows_split : rows]
+
+    # get the last column of X as the 'y' vector and split it into train and test sets
+    df_y_train = df[df.columns[cols - 1]].loc[0 : rows_split] 
+    df_y_test = df[df.columns[cols - 1]].loc[rows_split : rows]
+
+    return df_X_train, df_X_test, df_y_train, df_y_test
+
+
+def RidgeRegression(X, y, l:int = 2):
+
+    """
+    This function implements the ℓ2 -regularized least squares linear regression algorithm (aka ridge regression) which takes the form:
+    wRR=argminw(∥y−Xw∥^2 ) + λ * ∥w∥^2.
+    
+    Inputs:
+    y : vector 'y' from the split dataset
+    X : matrix 'X' from the split dataset
+    l : an arbitray value for lambda "λ"
+    
+    Outputs:
+    wRR : calculated as expressed above
+    deg_f : degrees of freedom as a function of lambda
+    """
+    #wRR = np.argmin((y - np.dot(X, w))**2) + l * w**2
+
+    xtranspose = np.transpose(X)
+    xtransx = np.dot(xtranspose, X)
+    if xtransx.shape[0] != xtransx.shape[1]:
+        raise ValueError('Needs to be a square matrix for inverse')
+    lamidentity = np.identity(xtransx.shape[0]) * l
+    matinv = np.linalg.inv(lamidentity + xtransx)
+    xtransy = np.dot(xtranspose, y)
+    wRR = np.dot(matinv, xtransy)
+    _, S, _ = np.linalg.svd(X)
+    deg_f = np.sum(np.square(S) / (np.square(S) + l))
+
+    return wRR, deg_f
+
+# implementation of gradient descent algorithm  
+def gradientDescent(x, y, theta, alpha, num_iters, c):
+    # get the number of samples in the training
+    m = x.shape[0]
+    
+    for i in range(0, num_iters):
+        
+        # find linear regression equation value, X and theta
+        z = np.dot(x, theta)
+        
+        # get the sigmoid of z
+        h = sigmoid(z)
+ 
+        # calculate the cost function, log loss
+        #J = (-1/m) * (np.dot(y.T, np.log(h)) + np.dot((1 - y).T, np.log(1-h)))
+        
+        # let's add L2 regularization
+        # c is L2 regularizer term
+        J = (-1/m) * ((np.dot(y.T, np.log(h)) + np.dot((1 - y).T, np.log(1-h))) + (c * np.sum(theta)))
+        
+        # update the weights theta
+        theta = theta - (alpha / m) * np.dot((x.T), (h - y))
+   
+    J = float(J)
+    return J, theta
+
+
+def write_csv(filename, a, b, c):
+        # write the outputs csv file
+        filepath = os.path.join(os.getcwd(),'datasets','out', filename)
+        df_a = pd.DataFrame(a)
+        df_b = pd.DataFrame(b)
+        df_c = pd.DataFrame(c)
+        df = pd.concat([df_a, df_b, df_c], axis = 1, ignore_index = True)
+        #dataframe = df.rename(columns={0:'alpha',1:'number_of_iterations',2:'b_0', 3:'b_age',4:'b_weight'})
+        df.to_csv(filepath, index = False, header = False)
+        return print("New Outputs file saved to: <<", filename, ">>", sep='', end='\n')
+
+
+def main():
+    """
+    ## $ python3 problem3.py input3.csv output3.csv
+    """
+    #take string for input data csv file
+    #in_data = str(sys.argv[1])
+    in_data = 'forestfires.csv'
+    #take string for output data csv file
+    #out_data = str(sys.argv[2])
+    out_data = 'output3.csv'
+
+    df = get_data(in_data)
+    # split the dataset
+    df_X_train, df_X_test, df_y_train, df_y_test = split_data(df, ratio = 0.7)
+
+    vis_data(df_X_train, df_y_train)
+
+    # TODO: function for the following iteration?
+    # solve the linear regression (ridge regression) for a varying lambda parameter
+    wRR_list = []
+    deg_f_list = []
+
+    # TODO: resolve issue with dimensions of dataframes, dotproduct, etc
+    for l_i in range(0,5000,1):
+        wRR, deg_f = RidgeRegression(df_X_train,df_y_train,l_i)
+        wRR_list.append(wRR)
+        deg_f_list.append(deg_f)
+
+    #TODO: plot results
+    
+if __name__ == '__main__':
+    main()
